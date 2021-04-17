95c8d4619859938d508b061b9eae99b07d91c438
leov1963 <leov1963@gmail.com>
2021-04-17 00:43:35 -0700

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -16,34 +16,39 @@ const Nav = () => (
         <Container>
                 <Menu.Item as={ Link } name='landingpage' to='' header>
                 <Image size='mini' src={logo} style={{ marginRight: '1.5em' }} />
-                Leo Vincent
+                Leo<br />Vincent
                 </Menu.Item>
 
-            <Dropdown item simple text='More'>
-            <Dropdown.Menu>
-                <Dropdown.Item as={ Link } name='aboutme' to='aboutme'>About Me</Dropdown.Item>
-                <Dropdown.Divider />
-                
-                <Dropdown.Item>
-                <i className='dropdown icon' />
-                <span className='text'>Projects</span>
+            {/* <Menu.Menu > */}
+                <Dropdown item simple text='More'>
                 <Dropdown.Menu>
-                    <Dropdown.Item>Pitchy to Perfect</Dropdown.Item>
-                    <Dropdown.Item>Write to Relax</Dropdown.Item>
-                    <Dropdown.Item>Mr. Namesy</Dropdown.Item>
-                    <Dropdown.Item>Grow a Skeleton</Dropdown.Item>
+                <Menu.Item as={ Link } name='aboutme' to='aboutme'>About Me</Menu.Item>
+                        <Dropdown.Divider />
+                    <Dropdown.Item>
+                        <i className='dropdown icon' />
+                        <span className='text'>Projects</span>
+                        <Dropdown.Menu>
+                            <Dropdown.Item>Pitchy to Perfect</Dropdown.Item>
+                            <Dropdown.Item>Write to Relax</Dropdown.Item>
+                            <Dropdown.Item>Mr. Namesy</Dropdown.Item>
+                            <Dropdown.Item>Grow a Skeleton</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown.Item>
+                    <Dropdown.Item>
+                    <i className='dropdown icon' />
+                    <span className='text'>Links</span>
+                    <Dropdown.Menu>
+                        <Dropdown.Item>
+                            <Button color='linkedin'><Icon name='linkedin' />My LinkedIn</Button>
+                        </Dropdown.Item>
+                        <Dropdown.Item>
+                            <Button color='github'><Icon name='github' />My Github</Button>
+                        </Dropdown.Item>
+                    </Dropdown.Menu>
+                    </Dropdown.Item>
                 </Dropdown.Menu>
-                </Dropdown.Item>
-                <Dropdown.Item>
-                <i className='dropdown icon' />
-                <span className='text'>Links</span>
-                <Dropdown.Menu>
-                    <Button color='linkedin'><Icon name='linkedin' />My LinkedIn</Button>
-                    <Button color='github'><Icon name='github' />My Github</Button>
-                </Dropdown.Menu>
-                </Dropdown.Item>
-            </Dropdown.Menu>
-            </Dropdown>
+                </Dropdown>
+            {/* </Menu.Menu> */}
         </Container>
         </Menu>
     </div>
