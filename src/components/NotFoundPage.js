7f73f46ed731437854110d39b6d2419d58616d0a
leov1963 <leov1963@gmail.com>
2021-04-16 17:48:16 -0700

    Add NotFoundPage.js

---
+++ src/components/NotFoundPage.js
@@ -0,0 +1,33 @@
+import React, { Fragment } from 'react';
+import { Header, Grid, Segment } from 'semantic-ui-react'
+import cuteFox from '../assets/cute-fox.gif'
+
+
+const NotFoundPage = () => {
+
+    return (
+        
+        <div>
+            <Fragment>
+                <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+                    <Grid.Column style={{ maxWidth: 450 }}>
+                        <div className="scores-container">
+                            <Header as="h2">Page not found!</Header>
+                            <Segment>
+                                <br />
+                                <div className="not-found-message">
+                                    Looks like you're a bit lost buddy!
+                                    <br />
+                                    Let's turn around and go to <a href='/login'>login</a>.
+                                </div>
+                                <img src={cuteFox} alt="cute fox" />
+                            </Segment>
+                        </div>                       
+                    </Grid.Column>
+                </Grid>
+            </Fragment>
+        </div>
+    );
+}
+
+export default NotFoundPage;
