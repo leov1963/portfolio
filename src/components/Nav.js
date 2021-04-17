509a7bb133e5639ef9addb7d4dc89eabf1c06354
leov1963 <leov1963@gmail.com>
2021-04-16 17:49:12 -0700

    Add Links and update menu items in Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import {
     Container,
     Dropdown,
     Image,
     Menu,
+    Icon,
+    Button
 } from 'semantic-ui-react'
 import logo from '../assets/logo.png'
 
@@ -11,29 +14,32 @@ const Nav = () => (
     <div>
         <Menu fixed='top' inverted>
         <Container>
-            <Menu.Item as='a' header>
-            <Image size='mini' src={logo} style={{ marginRight: '1.5em' }} />
-            Leo Vincent
-            </Menu.Item>
+                <Menu.Item as={ Link } name='landingpage' to='' header>
+                <Image size='mini' src={logo} style={{ marginRight: '1.5em' }} />
+                Leo Vincent
+                </Menu.Item>
 
             <Dropdown item simple text='More'>
             <Dropdown.Menu>
-                <Dropdown.Item>About Me</Dropdown.Item>
+                <Dropdown.Item as={ Link } name='aboutme' to='aboutme'>About Me</Dropdown.Item>
                 <Dropdown.Divider />
                 
                 <Dropdown.Item>
                 <i className='dropdown icon' />
                 <span className='text'>Projects</span>
                 <Dropdown.Menu>
-                    <Dropdown.Item>List Item</Dropdown.Item>
-                    <Dropdown.Item>List Item</Dropdown.Item>
+                    <Dropdown.Item>Pitchy to Perfect</Dropdown.Item>
+                    <Dropdown.Item>Write to Relax</Dropdown.Item>
+                    <Dropdown.Item>Mr. Namesy</Dropdown.Item>
+                    <Dropdown.Item>Grow a Skeleton</Dropdown.Item>
                 </Dropdown.Menu>
                 </Dropdown.Item>
                 <Dropdown.Item>
                 <i className='dropdown icon' />
                 <span className='text'>Links</span>
                 <Dropdown.Menu>
-                    <Dropdown.Item>List Item</Dropdown.Item>
+                    <Button color='linkedin'><Icon name='linkedin' />My LinkedIn</Button>
+                    <Button color='github'><Icon name='github' />My Github</Button>
                 </Dropdown.Menu>
                 </Dropdown.Item>
             </Dropdown.Menu>
